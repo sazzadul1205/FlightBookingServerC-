d6e4bd3dae3688ff0cb5441b4d7b387007b1d857
Pritom <psazzadul@gmail.com>
2026-07-27 11:25:45 +0600

    - iInitialized The C# Project

---
+++ Models/MarkupCommission.cs
@@ -0,0 +1,19 @@
+namespace FlightBooking.Models
+{
+    public class MarkupCommission
+    {
+        public int Id { get; set; }
+
+        public int? UserId { get; set; }
+
+        public string? AirlineCode { get; set; }
+
+        public string MarkupType { get; set; }
+        public string CommissionType { get; set; }
+        public decimal Markup { get; set; }
+        public decimal Commission { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
